6539ba67defce63fa1e38d744af374b50e489def
Dulguun Battulga <dolugen@users.noreply.github.com>
2020-05-31 15:08:31 +0200

    fix imports and exports

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
-from sqlalchemy import Column, String, create_engine
-from flask_sqlalchemy import SQLAlchemy
+import os
 import json
+from sqlalchemy import Column, String, Integer, create_engine
+from flask_sqlalchemy import SQLAlchemy
 
 database_path = os.environ['DATABASE_URL']
 
